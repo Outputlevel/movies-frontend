4187e593221f380804ab0a7416b41ca011531267
unknown <rix.mtz@gmail.com>
2024-04-26 21:29:31 -0500

    .env removed

--- src/services/getMovies.js
+++ src/services/getMovies.js
@@ -1,6 +1,7 @@
 export async function getMovies() {
     try {
-        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/movies`);
+        //const response = await fetch(`${import.meta.env.VITE_API_URL}/api/movies`);
+        const response = await fetch("https://api.outputlevel.com/api/movies");
         if (!response.ok) {
             throw new Error('Failed to fetch movies');
         }
